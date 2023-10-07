10a42af25153337c503f0b19d596feb565ca3264
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2023-10-07 23:03:35 +0300

    restfull endpoint finalizing

--- src/main/java/com/ibra/training/controller/UserController.java
+++ src/main/java/com/ibra/training/controller/UserController.java
@@ -2,8 +2,11 @@ package com.ibra.training.controller;
 
 import com.ibra.training.model.User;
 import com.ibra.training.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequestMapping("/users")
 @RestController
@@ -17,9 +20,38 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public User getUser(HttpServletResponse response, @PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+        return user.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(HttpServletResponse response, @PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        userRepository.deleteById(id);
+    }
+
     @PostMapping("/")
-    public User save(@RequestBody String name) {
-        return userRepository.save(new User(name));
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User update(HttpServletResponse response, @PathVariable("id") Long userId, @RequestBody User user) {
+        Optional<User> oldUser = userRepository.findById(userId);
+        if(oldUser.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+        return userRepository.save(user);
     }
 
 }
